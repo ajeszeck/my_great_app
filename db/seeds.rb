b5dfb989b2c240e14f1332dff4e6fc68ea8ec55d
Anna J <ajeszeck@gmail.com>
2017-10-15 17:07:12 -0700

    Add models and migrations for posts and users.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+10.times do 
+  User.create(name: Faker::Name.name_with_middle, email: Faker::Internet.safe_email)
+end
+
+20.times do 
+  Post.create(title: "#{Faker::Dessert.flavor} #{Faker::Dessert.topping}", description: Faker::Lorem.paragraph(2), user_id: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0].sample)
+end
